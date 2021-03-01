db8074eb67bf7595898b19764d30c2223628f40b
Aurorah Harmony <mineeraft@gmail.com>
2021-03-01 13:51:26 +1300

    Load parent first and check version

--- functions.php
+++ functions.php
@@ -5,5 +5,12 @@ add_action('wp_enqueue_scripts', 'my_theme_enqueue_styles');
 function my_theme_enqueue_styles()
 {
   wp_enqueue_style('parent-style', get_template_directory_uri() . "/style.css");
-  wp_enqueue_style('child-style', get_stylesheet_directory_uri() . "/style.css");
+  wp_enqueue_style(
+    'child-style',
+    get_stylesheet_directory_uri() . "/style.css",
+    //Force parent style to load first
+    ['parent-style'],
+    //Ensure the theme version is the same as the cached version
+    wp_get_theme()->get('Version')
+  );
 }
