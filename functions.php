14899d78dcb86b20e04977b9ec6e7929006bec26
Aurorah Harmony <mineeraft@gmail.com>
2021-03-01 13:48:33 +1300

    Enqueue stylesheets

---
+++ functions.php
@@ -0,0 +1,9 @@
+<?php
+
+//Load the style sheets
+add_action('wp_enqueue_scripts', 'my_theme_enqueue_styles');
+function my_theme_enqueue_styles()
+{
+  wp_enqueue_style('parent-style', get_template_directory_uri() . "/style.css");
+  wp_enqueue_style('child-style', get_stylesheet_directory_uri() . "/style.css");
+}
