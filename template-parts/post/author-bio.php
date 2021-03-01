7036818e5518a4bc20f278a461464d9eec8ecb52
Aurorah Harmony <mineeraft@gmail.com>
2021-03-01 15:20:21 +1300

    Alter author bio

--- template-parts/post/author-bio.php
+++ template-parts/post/author-bio.php
@@ -15,7 +15,7 @@ if ((bool) get_the_author_meta('description')) : ?>
       <?php
 				printf(
 					/* translators: %s: Post author. */
-					__('Published by %s', 'twentynineteen'),
+					__('Published by %s ', 'twentynineteen'),
 					esc_html(get_the_author())
 				);
 
@@ -25,9 +25,7 @@ if ((bool) get_the_author_meta('description')) : ?>
   </h2>
   <p class="author-description">
     <?php the_author_meta('description'); ?>
-    <a class="author-link" href="<?php echo esc_url(get_author_posts_url(get_the_author_meta('ID'))); ?>" rel="author">
-      <?php _e('View more posts', 'twentynineteen'); ?>
-    </a>
+
   </p><!-- .author-description -->
 </div><!-- .author-bio -->
 <?php endif; ?>
